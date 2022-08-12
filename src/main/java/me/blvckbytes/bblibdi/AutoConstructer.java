b58f3cf461519e73dcca84978a1d2b9896891854
Philip Hell <blvckbytes@gmail.com>
2022-08-12 05:02:37 +0200

    added license

--- src/main/java/me/blvckbytes/bblibdi/AutoConstructer.java
+++ src/main/java/me/blvckbytes/bblibdi/AutoConstructer.java
@@ -29,6 +29,19 @@ import java.util.stream.Collectors;
   parameter always needs to have a corresponding class member field of the
   same type, into which the value gets injected as soon as it's available,
   where the constructor will be passed a null-value for it in the mean time.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 public class AutoConstructer implements IAutoConstructer {
 
